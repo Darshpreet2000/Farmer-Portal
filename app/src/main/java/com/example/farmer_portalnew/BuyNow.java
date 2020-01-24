4c54ff2d2095dd0db3392723f8d9518a510384ec
Darshpreet2000 <47180477+Darshpreet2000@users.noreply.github.com>
2020-01-24 13:56:04 +0530

    Changes packagename

---
+++ app/src/main/java/com/example/farmer_portalnew/BuyNow.java
@@ -0,0 +1,27 @@
+package com.example.farmer_portalnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.farmer_portalnew.PaymentMethod.PaymentsMethod;
+
+public class BuyNow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_now);
+        Button buynow=findViewById(R.id.buynow);
+        buynow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(BuyNow.this, PaymentsMethod.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
